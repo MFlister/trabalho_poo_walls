7b2b1f1efb4e0029183e49587db1c5af20248b57
rosi-pc <rosimeirepereira777@gmail.com>
2022-01-14 19:39:29 -0300

    alterações no arquivo task

--- task.py
+++ task.py
@@ -23,103 +23,97 @@ class Task:
             self.tag = tag
             self.status = 'Pendente'
             self.target_date = target_date.strftime("%d/%m/%Y")
-            
-            
-            
+
 # Classe Gerenciador de Tarefas
+
 
 class TaskManager:
 
     def __init__(self, url='/tarefas.csv'):
         self.url = url
+        self.directory_csv = os.path.dirname(
+            os.path.abspath(__file__)) + self.url
 
     # Adiciona Task
     def add(self, task):
         df = pd.DataFrame(task.__dict__, index=[0])
 
-        if os.path.isfile("."+self.url):
+        if os.path.isfile(self.directory_csv):
 
-            tasks = pd.read_csv(os.path.dirname(
-                os.path.abspath(__file__)) + self.url)
+            tasks = pd.read_csv(self.directory_csv)
 
             if not tasks.loc[tasks['title'] == task.title].empty:
-                print("\nTarefa já existente\n")
+                print("\033[31m"+"\nTarefa já existente\n"+"\033[31m")
                 return None
 
-            df.to_csv(os.path.dirname(
-                os.path.abspath(__file__)) + self.url, mode='a', header=None, index=False)
+            df.to_csv(self.directory_csv, mode='a', header=None, index=False)
         else:
-            df.to_csv(os.path.dirname(
-                os.path.abspath(__file__)) + self.url, mode='a', index=False)
+            df.to_csv(self.directory_csv, mode='a', index=False)
 
-        print("\nTarefa Adicionada com Sucesso\n")
+        print("\033[32m" + "\nTarefa Adicionada com Sucesso\n" + "\033[32m")
 
     # Atualiza Status da Task
     def updateStatus(self, title):
 
-        if not os.path.isfile("."+self.url):
-            print("\nLista de Tarefas Inexistente: cadastre alguma tarefa\n")
+        if not self.__CSVFileExist():
             return None
 
-        tasks = pd.read_csv(os.path.dirname(
-            os.path.abspath(__file__)) + self.url)
+        tasks = pd.read_csv(self.directory_csv)
 
         if tasks.empty:
-            print("\nLista de Tarefas Vazia\n")
+            print("\033[31m"+"\nLista de Tarefas Vazia\n"+"\033[31m")
+            return None
+
+        if tasks.loc[tasks['title'] == title].empty:
+            print("\033[31m" +
+                  "\nNão há nenhuma tarefa com este título\n"+"\033[31m")
         else:
 
-            if tasks.loc[tasks['title'] == title].empty:
-                print("\nNão há nenhuma tarefa com este título\n")
-            else:
+            current_status = tasks.loc[tasks['title'] == title]['status']
 
-                current_status = tasks.loc[tasks['title'] == title]['status']
+            print(f"\033[33m \nTarefa '{title}': {current_status.iloc[0]}\n \033[33m")
 
-                print(f"\nTarefa '{title}': {current_status.iloc[0]}\n")
+            choice_status = self.__showChoiceStatus()
+            tasks.loc[tasks['title'] == title, 'status'] = choice_status
+            tasks.to_csv(self.directory_csv, index=False)
 
-                choice_status = self.__showChoiceStatus()
-                tasks.loc[tasks['title'] == title, 'status'] = choice_status
-                tasks.to_csv(os.path.dirname(
-                    os.path.abspath(__file__)) + self.url, index=False)
-
-                print("\nStatus Atualizado\n")
+            print("\033[32m"+"\nStatus Atualizado\n"+"\033[32m")
 
     # Deleta a Task
     def deleteTask(self, title):
 
-        if not os.path.isfile("."+self.url):
-            print("\nLista de Tarefas Inexistente: cadastre alguma tarefa\n")
+        if not self.__CSVFileExist():
             return None
 
-        tasks = pd.read_csv(os.path.dirname(
-            os.path.abspath(__file__)) + self.url)
+        tasks = pd.read_csv(self.directory_csv)
 
-        if tasks.empty or not os.path.isfile("."+self.url):
-            print("\nLista de Tarefas Vazia\n")
+        if tasks.empty:
+            print("\033[31m"+"\nLista de Tarefas Vazia\n"+"\033[31m")
+            return None
+
+        if tasks.loc[tasks['title'] == title].empty:
+            print("\033[31m" +
+                  "\nNão há nenhuma tarefa com este título\n"+"\033[31m")
         else:
-            if tasks.loc[tasks['title'] == title].empty:
-                print("\nNão há nenhuma tarefa com este título\n")
-            else:
-                task = tasks.loc[tasks['title'] == title]
-                tasks.drop(task.index, inplace=True)
-                tasks.to_csv(os.path.dirname(
-                    os.path.abspath(__file__)) + self.url, index=False)
-                
-                print("\nTarefa deletada com sucesso\n")
+            task = tasks.loc[tasks['title'] == title]
+            tasks.drop(task.index, inplace=True)
+            tasks.to_csv(self.directory_csv, index=False)
+
+            print("\033[32m"+"\nTarefa deletada com sucesso\n"+"\033[32m")
 
     def showTasks(self, date=None):
-        if not os.path.isfile("."+self.url):
-            print("\nLista de Tarefas Inexistente: cadastre alguma tarefa\n")
+
+        if not self.__CSVFileExist():
             return None
 
-        tasks = pd.read_csv(os.path.dirname(
-            os.path.abspath(__file__)) + self.url)
-        
+        tasks = pd.read_csv(self.directory_csv)
+
+        if tasks.empty:
+            print("\033[31m"+'\nNão há nada para ser visualizado\n'+"\033[31m")
+            return None
+
         if date != '':
             tasks = tasks.loc[tasks['target_date'] == date]
-
-            if tasks.empty:
-                print('\nNão há nada para ser visualizado\n')
-                return None
 
         print("\n################### Tarefas ###################")
 
@@ -135,8 +129,17 @@ class TaskManager:
             ''')
         print("###############################################\n")
 
+    def __CSVFileExist(self):
+        if not os.path.isfile(self.directory_csv):
+            print(
+                "\033[31m"+"\nLista de Tarefas Inexistente: cadastre alguma tarefa\n"+"\033[31m")
+            return False
+        else:
+            return True
+
     # Mostra as opções de mudar o status
-    
+   
+   
     @staticmethod
     def __showChoiceStatus():
         questions = [
@@ -151,39 +154,44 @@ class TaskManager:
         return answers['status']
 
     # Input dados da Task do usuário
-    
+   
     @staticmethod
     def getTask():
-        questions = [
-            inquirer.Editor(name='title', message='Título'),
-            inquirer.Editor(name='description', message='Descrição'),
-            Text(
-                name='tag', message='Tag (deixe em branco caso não queira colocar uma tag)'),
-            Text(name='date', message='Data (deixe em branco caso a data seja hoje - formato: dd/mm/AAAA)')
-        ]
 
-        return inquirer.prompt(questions)
+        data = dict()
+
+        title = input("[?] Título: ")
+        description = input('[?] Descrição: ')
+        tag = input(
+            '[?] Tag (deixe em branco caso não queira colocar uma tag): ')
+        date = input(
+            '[?] Data (deixe em branco caso a data seja hoje - formato: dd/mm/AAAA): ')
+
+        data['title'] = title
+        data['description'] = description
+        data["tag"] = tag
+        data['date'] = date
+
+        return data
 
     # Input do título pelo usuário
-    
     @staticmethod
     def getTitle():
 
-        questions = [
-            inquirer.Text(name='title', message='Digite o título da tarefa')
-        ]
+        title = input('\nDigite o título da tarefa: ')
 
-        answers = inquirer.prompt(questions)
-
-        return answers['title']
+        return title
 
     @staticmethod
     def getDate():
 
         questions = [
-            inquirer.Text(name='date', message='Digite a data a ser filtrado (deixe em branco caso queira mostrar todos)')
+            inquirer.Text(
+                name='date', message='Digite a data a ser filtrado (deixe em branco caso queira mostrar todos)')
         ]
 
         answers = inquirer.prompt(questions)
 
         return answers['date']
+
+
